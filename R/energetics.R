afa00f999da2609c4ad3d7ce501a486c581ace3e
James Eapen <james.eapen@vai.org>
2023-05-20 09:55:20 -0400

    chore: move functions to separate files

--- R/energetics.R
+++ R/energetics.R
@@ -1,26 +1,3 @@
-#' Input data TODO
-#'
-#' Reads input data
-#' @param input_data The name of the sample file
-#' @param sheet The number of the sheet containing the data. Default is 2
-#' because the output from Seahorse Wave is on sheet 2
-#' @return a seahorse_rates table
-#'
-#' @importFrom data.table as.data.table
-#' @importFrom data.table :=
-#' @importFrom data.table tstrsplit
-#' @importFrom readxl read_excel
-#' @export
-#'
-#' @examples
-#' seahorse_rates <- read_data("sample1")
-
-read_data <- function(input_data, sheet = 2) {
-  as.data.table(read_excel(input_data, sheet))[
-  , c("cell_line", "assay_type") := tstrsplit( Group, " ", fixed = TRUE)
-  ][]
-}
-
 #' Get energetics list
 #'
 #' Makes list of energetics tables
