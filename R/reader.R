cfa9af9b3835c70978f8d463f411a349c5828ae5
James Eapen <james.eapen@vai.org>
2023-05-20 11:17:41 -0400

    fix(reader): allow reading multiple replicate excel files

--- R/reader.R
+++ R/reader.R
@@ -1,23 +1,25 @@
 #' Input data TODO
 #'
 #' Reads input data
-#' @param input_data The name of the sample file
+#' @param rep_list A list of paths to the excel files. One file per replicate
 #' @param sheet The number of the sheet containing the data. Default is 2
 #' because the output from Seahorse Wave is on sheet 2
 #' @return a seahorse_rates table
 #'
-#' @importFrom data.table as.data.table
-#' @importFrom data.table :=
-#' @importFrom data.table tstrsplit
+#' @importFrom data.table setDT := tstrsplit rbindlist
 #' @importFrom readxl read_excel
 #' @export
 #'
 #' @examples
 #' seahorse_rates <- read_data("sample1")
 
-read_data <- function(input_data, sheet = 2) {
-  as.data.table(read_excel(input_data, sheet))[
-  , c("cell_line", "assay_type") := tstrsplit( Group, " ", fixed = TRUE)
-  ][]
+read_data <- function(rep_list, sheet = 2) {
+  # TODO: sanity check for column names
+  reps <- lapply(seq.int(length(rep_list)), function(i) {
+    setDT(read_excel(rep_list[i], sheet))[
+    , c("cell_line", "assay_type") := tstrsplit(Group, " ", fixed = TRUE)
+    ][, replicate := i][, Group := NULL]
+  })
+  rbindlist(reps)
 }
 
