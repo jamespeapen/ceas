997316a10f24d2b857773c018d95f589b4d3d0bd
James Eapen <39574560+jamespeapen@users.noreply.github.com>
2023-07-08 17:20:07 -0400

    fix(bioscope plot): add shape and size option
    Use `labs` for legend title

--- R/bioscope_plot.R
+++ R/bioscope_plot.R
@@ -4,7 +4,7 @@
 #' @param energetics_summary A summary table of ATP values
 #' @return a ggplot
 #'
-#' @importFrom ggplot2 ggplot aes geom_point labs xlab ylab geom_linerange xlim ylim scale_color_discrete
+#' @importFrom ggplot2 ggplot aes geom_point labs xlab ylab geom_linerange xlim ylim
 #' @export
 #'
 #' @examples
@@ -14,7 +14,19 @@
 #'                   get_energetics(ph, pka, buffer) |>
 #'                   get_energetics_summary()
 #' bioscope_plot(seahorse_rates)
-bioscope_plot <- function(energetics_summary) {
+#'
+#' # to change fill, the geom_point shape number should be between 15 and 25
+#' bioscope_plot(seahorse_rates, shape = 21) + # filled circle
+#' ggplot2::scale_fill_manual(values = c("#e36500", "#b52356", "#3cb62d", "#328fe1")
+#'
+#' # to change color use ggplot2::scale_color_manual
+#' bioscope_plot(seahorse_rates) +
+#' ggplot2::scale_color_manual(values = c("#e36500", "#b52356", "#3cb62d", "#328fe1")
+bioscope_plot <- function(
+  energetics_summary,
+  size = 2,
+  shape = 21
+) {
 
   # sanity checks
   data_cols <- c(
@@ -36,15 +48,22 @@ bioscope_plot <- function(energetics_summary) {
       max(energetics_summary$glyc_max_glyc.mean, energetics_summary$ox_max_ox.mean)
     ), -3)
 
-  ggplot(energetics_summary, aes(glyc_max_glyc.mean, ox_max_ox.mean, color = cell_line)) +
-  geom_point() +
+  ggplot(energetics_summary, aes(
+    glyc_max_glyc.mean,
+    ox_max_ox.mean,
+    color = cell_line,
+    fill = cell_line
+  )) +
+  geom_point(shape = shape, size = size) +
   geom_point(
     data = energetics_summary,
-    aes(x = glyc_no_drugs.mean, y = ox_no_drugs.mean, color = cell_line)
+    aes(x = glyc_no_drugs.mean, y = ox_no_drugs.mean, color = cell_line),
+    size = size,
+    shape = shape
   ) +
   xlab("ATP Production from Glycolysis (JATP)") +
   ylab("ATP Production from OXPHOS (JATP)") +
-  scale_color_discrete(name = "Cell Line") +
+  labs(color = "Cell line", fill = "Cell line") +
   xlim(0, max_axis) + ylim(0, max_axis) +
   geom_linerange(aes(
     x = glyc_max_glyc.mean, y = ox_max_ox.mean,
