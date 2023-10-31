b5725fc28c6d738916f3c669a325fa3a211f11dd
James Eapen <james.eapen@vai.org>
2023-10-31 15:16:53 -0400

    docs(read_data): update wording for getting a list of excel files

--- R/reader.R
+++ R/reader.R
@@ -2,10 +2,9 @@
 #'
 #' Reads input seahore data from an excel Seahorse Wave File
 #' @param rep_list A list of Seahorse Wave excel export files. One file per
-#' replicate. Group all replicates for a given experiment in a single folder,
-#' and write that folder's path in "seahorse_data". You can use
-#' `list.files("seahorse_data") "full.names=TRUE") to get the paths to the
-#' files.
+#' replicate. If your data is in a directory called "seahorse_data", use
+#' `list.files("seahorse_data", pattern = "*.xlsx", full.names = TRUE)` to make
+#' a list of the excel files.
 #' @param sheet The number of the excel sheet containing the long-form Seahorse
 #' data. Default is 2 because the long-form output from Seahorse Wave is on
 #' sheet 2
