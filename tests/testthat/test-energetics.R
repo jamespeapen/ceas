1d62304e27a65646cd4cd5dea120912b3d03d9de
James Eapen <james.eapen@vai.org>
2024-08-01 10:04:26 -0400

    test(energetics): update test with replicate column

--- tests/testthat/test-energetics.R
+++ tests/testthat/test-energetics.R
@@ -1,15 +1,17 @@
+library(data.table)
 get_energetics_input <- list(
-  basal = data.table::data.table(OCR = c(200, 300, 400), ECAR = c(20, 35, 50), assay_type = rep("MITO", 3), exp_group = c("a", "b", "c")),
-  nonmito = data.table::data.table(OCR = c(30, 45, 60), ECAR = c(40, 55, 70), assay_type = rep("MITO"), exp_group = c("a", "b", "c")),
-  max_glyc = data.table::data.table(OCR = c(200, 300, 400), ECAR = c(50, 70, 90), assay_type = rep("GLYCO", 3), exp_group = c("a", "b", "c")),
-  uncoupled = data.table::data.table(OCR = c(50, 60, 70), ECAR = c(30, 40, 50), assay_type = rep("MITO", 3), exp_group = c("a", "b", "c")),
-  no_glucose_glyc = data.table::data.table(OCR = c(200, 300, 400), ECAR = c(10, 20, 30), assay_type = rep("GLYCO", 3), exp_group = c("a", "b", "c")),
-  maxresp = data.table::data.table(OCR = c(300, 400, 500), ECAR = c(30, 40, 50), assay_type = rep("GLYCO", 3), exp_group = c("a", "b", "c")),
-  glucose_glyc = data.table::data.table(OCR = c(200, 300, 400), ECAR = c(30, 40, 50), assay_type = rep("GLYCO", 3), exp_group = c("a", "b", "c"))
+  basal = data.table(OCR = c(200, 300, 400), ECAR = c(20, 35, 50), assay_type = rep("MITO", 3), exp_group = c("a", "b", "c"), replicate = rep(1, 3)),
+  nonmito = data.table(OCR = c(30, 45, 60), ECAR = c(40, 55, 70), assay_type = rep("MITO"), exp_group = c("a", "b", "c"), replicate = rep(1, 3)),
+  max_glyc = data.table(OCR = c(200, 300, 400), ECAR = c(50, 70, 90), assay_type = rep("GLYCO", 3), exp_group = c("a", "b", "c"), replicate = rep(1, 3)),
+  uncoupled = data.table(OCR = c(50, 60, 70), ECAR = c(30, 40, 50), assay_type = rep("MITO", 3), exp_group = c("a", "b", "c"), replicate = rep(1, 3)),
+  no_glucose_glyc = data.table(OCR = c(200, 300, 400), ECAR = c(10, 20, 30), assay_type = rep("GLYCO", 3), exp_group = c("a", "b", "c"), replicate = rep(1, 3)),
+  maxresp = data.table(OCR = c(300, 400, 500), ECAR = c(30, 40, 50), assay_type = rep("GLYCO", 3), exp_group = c("a", "b", "c"), replicate = rep(1, 3)),
+  glucose_glyc = data.table(OCR = c(200, 300, 400), ECAR = c(30, 40, 50), assay_type = rep("GLYCO", 3), exp_group = c("a", "b", "c"), replicate = rep(1, 3))
 )
 
 get_energetics_expected <- data.table::data.table(
   exp_group = factor(c("a", "b", "c")),
+  replicate = as.factor(rep(1, 3)),
   ATP_basal_resp = c(862.5056, 1375.8950, 1889.2844),
   ATP_max_resp = c(886.7056, 1400.0950, 1913.4844),
   ATP_basal_glyc = c(194.7699, 242.1549, 289.5398),
